22bedffc9ffe1013b38b3a47ad8b0c660cb6fb27
Dominik Szopa <dominik@openroad.ca>
2018-12-01 01:30:08 -0800

    Add donation processing of the submitted form

--- team_fundraising/urls.py
+++ team_fundraising/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index_view, name='index'),
     path('fundraiser/<int:fundraiser_id>/', views.fundraiser_view, name='fundraiser'),
     path('donate/<int:fundraiser_id>/', views.donate_view, name="donate"),
+    path('donate_post/<int:fundraiser_id>/', views.donate_post, name="donate_post"),
 ]
--- team_fundraising/views.py
+++ team_fundraising/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from datetime import datetime
 
 from .models import Campaign, Fundraiser, Donation
 
@@ -35,7 +36,38 @@ def fundraiser_view(request, fundraiser_id):
     return render(request, template, context)
 
 def donate_view(request, fundraiser_id):
+
     template = 'team_fundraising/donate.html'
+
+    fundraiser = get_object_or_404(Fundraiser, pk=fundraiser_id)
+
+    context = {
+        'fundraiser' : fundraiser,
+    }
+
+    return render(request, template, context)
+
+def donate_post(request, fundraiser_id):
+    """
+    Takes the submission of a donation form, and creates a new donation 
+    object to save to the model
+    """
+
+    donation = Donation()
+
+    donation.fundraiser = get_object_or_404(Fundraiser, pk=fundraiser_id)
+    donation.amount = float(request.POST['amount'])
+    donation.name = request.POST['name']
+    donation.message = request.POST['message']
+
+    if 'anonymous' in request.POST.keys() :
+        if request.POST['anonymous'] == "Yes" : 
+            donation.anonymous = True;
+
+    donation.date = datetime.now()
+    donation.save()
+
+    template = 'team_fundraising/donate_post.html'
 
     fundraiser = get_object_or_404(Fundraiser, pk=fundraiser_id)
 

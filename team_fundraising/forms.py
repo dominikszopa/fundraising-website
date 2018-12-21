b061e79142cefe31324665c497f09cdcd68e85be
Dominik Szopa <dominik@openroad.ca>
2018-12-21 01:39:29 -0800

    Added support for "other" amount

--- team_fundraising/forms.py
+++ team_fundraising/forms.py
@@ -8,24 +8,35 @@ class DonationForm(forms.Form):
     """ Form for a new Donation, which can be tied to a specific fundraiser """
 
     name = forms.CharField()
-    amount = forms.CharField()                                    
+    amount = forms.CharField()      
+    other_amount = forms.CharField(required=False)                              
     email = forms.EmailField()
     anonymous = forms.BooleanField(required=False)
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows' : 3})) 
+    
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'required' : False,
+             'rows' : 3}))
 
-    """
+    
     def clean(self):
         # Use the amount or "other amount" from the form
-        cleaned_data = super(DonationForm, self).clean()
 
-        amount = cleaned_data.get("amount")
-        other_amount = cleaned_data.get("other_amount")
+        try:
+            amount = self.cleaned_data['amount']
+        except KeyError as e:
+            amount = ""
+        
+        try:
+            other_amount = self.cleaned_data['other_amount']
+        except KeyError as e:
+            other_amount = ""
 
-        if amount == -1:
+        if amount == 'other' or amount == '':
             #TODO pull out just the number, in case they added a $
             try:
-                amount = float(other_amount)
+                self.cleaned_data['amount'] = float(other_amount)
             except ValueError:
-                amount = 0
-    """
+                raise forms.ValidationError("Amount is not a number")
+    
 

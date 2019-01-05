9b3ef57d87e584414c18c5bd683d8d751685acaf
Dominik Szopa <dominik@openroad.ca>
2019-01-05 00:44:34 -0800

    Remove and clean up old donation form

--- team_fundraising/urls.py
+++ team_fundraising/urls.py
@@ -8,10 +8,8 @@ app_name = 'team_fundraising'
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('fundraiser/<int:fundraiser_id>/', views.fundraiser_view, name='fundraiser'),
-    path('donate/<int:fundraiser_id>/', views.donate_view, name="donate"),
     path('donation/<int:fundraiser_id>/', views.new_donation, name="donation"),
-    path('donate_post/<int:fundraiser_id>/', views.donate_post, name="donate_post"),
+    path('update_fundraiser/', views.update_fundraiser, name="update_fundraiser"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name="signup"),
-    path('update_fundraiser/', views.update_fundraiser, name="update_fundraiser"),
 ]

885d847d684e133950797fcaa0034522f5a00e7b
Dominik Szopa <dominik@openroad.ca>
2019-01-11 02:23:31 -0800

    Linting a few more files

--- team_fundraising/forms.py
+++ team_fundraising/forms.py
@@ -2,47 +2,48 @@
 """
 from django import forms
 from datetime import datetime
-from django.forms import Form, Textarea, BooleanField, NumberInput
-from .models import Donation, Fundraiser
+from .models import Fundraiser
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
 
 class DonationForm(forms.Form):
     """ Form for a new Donation, which can be tied to a specific fundraiser """
 
     name = forms.CharField()
-    amount = forms.CharField()      
-    other_amount = forms.CharField(required=False)                              
+    amount = forms.CharField()
+    other_amount = forms.CharField(required=False)
     email = forms.EmailField()
     anonymous = forms.BooleanField(required=False)
     date = datetime.now()
-    
+
     message = forms.CharField(
         widget=forms.Textarea(
-            attrs={'required' : False,
-             'rows' : 3}))
+            attrs={'required': False, 'rows': 3}
+        )
+    )
 
-    
     def clean(self):
-    # Use the amount or "other amount" from the form
+        # Use the amount or "other amount" from the form
 
         try:
             amount = self.cleaned_data['amount']
         except KeyError as e:
             amount = ""
-        
+
         try:
             other_amount = self.cleaned_data['other_amount']
         except KeyError as e:
             other_amount = ""
 
         if amount == 'other' or amount == '':
-            #TODO pull out just the number, in case they added a $
+            # TODO pull out just the number, in case they added a $
             try:
                 amount = float(other_amount)
             except ValueError:
                 raise forms.ValidationError("Amount is not a number")
-    
+
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
@@ -66,7 +67,7 @@ class SignUpForm(UserCreationForm):
             user.save()
 
         return user
-    
+
 
 class FundraiserForm(forms.ModelForm):
     class Meta:
--- team_fundraising/urls.py
+++ team_fundraising/urls.py
@@ -7,9 +7,18 @@ app_name = 'team_fundraising'
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('fundraiser/<int:fundraiser_id>/', views.fundraiser_view, name='fundraiser'),
-    path('donation/<int:fundraiser_id>/', views.new_donation, name="donation"),
-    path('accounts/update_fundraiser/', views.update_fundraiser, name="update_fundraiser"),
+    path(
+        'fundraiser/<int:fundraiser_id>/',
+        views.fundraiser_view,
+        name='fundraiser'
+    ),
+    path(
+        'donation/<int:fundraiser_id>/', views.new_donation, name="donation"),
+    path(
+        'accounts/update_fundraiser/',
+        views.update_fundraiser,
+        name="update_fundraiser",
+    ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name="signup"),
 ]

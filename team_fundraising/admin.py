97a0d6af454119673cb4bc99a3fbc196be44f802
Dominik Szopa <techdomi@gmail.com>
2019-09-08 23:58:39 -0700

    Add more fields to Donation admin display

--- team_fundraising/admin.py
+++ team_fundraising/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import Campaign, Fundraiser, Donation
 
-# Register your models here.
 
 class CampaignAdmin(admin.ModelAdmin):
     pass
 
+
+# Register your models here.
+
 admin.site.register(Campaign)
 admin.site.register(Fundraiser)
-admin.site.register(Donation)
+# admin.site.register(Donation)
+
+
+@admin.register(Donation)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'date', 'amount', 'payment_status',
+        'email', 'fundraiser', 'country'
+        )
+    search_fields = ('name', 'email')
+    list_filter = ('payment_status', 'country')

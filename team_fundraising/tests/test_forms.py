e0bd03deacf7abdac75273ec6f00992dc78f95cf
Dominik Szopa <techdomi@gmail.com>
2019-10-19 01:43:28 -0700

    Added more form tests

--- team_fundraising/tests/test_forms.py
+++ team_fundraising/tests/test_forms.py
@@ -1,16 +1,13 @@
-from django.urls import reverse
 from .test_models import TestModels
-from ..forms import SignUpForm
-from ..models import Fundraiser
+from ..forms import SignUpForm, DonationForm, FundraiserForm
 
 
 class SignupTests(TestModels):
 
-    def test_signup_new_person(self):
+    def test_signup_valid(self):
         form = SignUpForm(
             data={
                 'name': 'Test Fundraiser',
-                'goal': 200,
                 'email': 'name@domain.com',
                 'username': 'username',
                 'password1': 'j234rlj33s',
@@ -19,12 +16,48 @@ class SignupTests(TestModels):
         )
         self.assertTrue(form.is_valid())
 
+    def test_signup_save(self):
+        form = SignUpForm(
+            data={
+                'name': 'Test Fundraiser',
+                'email': 'name@domain.com',
+                'username': 'username',
+                'password1': 'j234rlj33s',
+                'password2': 'j234rlj33s',
+                }
+            )
+        user = form.save()
+        self.assertEqual(user.username, 'username')
 
-class SignUpPosts(TestModels):
 
-    def test_post_signup_new_person(self):
-        response = self.client.post(
-            reverse('team_fundraising:signup'),
+class DonationFormTests(TestModels):
+
+    def test_simple_donation_valid(self):
+        form = DonationForm(
+            data={
+                'name': 'Donator',
+                'amount': '50',
+                'email': 'name@domain.com',
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_other_donation_valid(self):
+        form = DonationForm(
+            data={
+                'name': 'Donator',
+                'amount': 'other',
+                'other_amount': '33',
+                'email': 'name@domain.com',
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+
+class FundraiserFormTest(TestModels):
+
+    def test_simple_fundraiser(self):
+        form = FundraiserForm(
             data={
                 'campaign': '1',
                 'name': 'Test Post',
@@ -33,8 +66,6 @@ class SignUpPosts(TestModels):
                 'username': 'username',
                 'password1': 'j234rlj33s',
                 'password2': 'j234rlj33s',
-                }
+            }
         )
-        self.assertEqual(response.status_code, 200)
-        fundraiser = Fundraiser.objects.get(id=3)
-        self.assertEquals(str(fundraiser), 'First Fundraiser')
+        self.assertTrue(form.is_valid())

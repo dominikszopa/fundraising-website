4ed28c1fe60ed3054035fcec84f608ac88d07315
Dominik Szopa <techdomi@gmail.com>
2023-02-27 02:37:03 -0800

    Added validation & warning for spaces in username

--- team_fundraising/static/validation.js
+++ team_fundraising/static/validation.js
@@ -1,9 +1,9 @@
 
 function checkPasswordsMatch(password1, password2) {
 /**
- * Compares two password DOM objects and sets the second one 
+ * Compares two password DOM objects and sets the second one
  * invalid if they are not matching
- * 
+ *
  * @param password1 The primary password to match
  * @param password2 The second password that should be equal to password1
  */
@@ -22,3 +22,24 @@ function checkPasswordsMatch(password1, password2) {
 
 }
 
+function checkUsername(username) {
+/**
+ * Checks if a username is valid
+ * It can be an email, but cannot have spaces
+ */
+
+    var usernameval = username.val();
+    var usernameRegex = /^[a-zA-Z0-9_@.]{1,30}$/;
+
+    if(usernameRegex.test(usernameval)) {
+        username.removeClass("is-invalid");
+        username.addClass("is-valid");
+        return true;
+    } else {
+        username.addClass("is-invalid");
+        username.append("<div>Username must be between 3 and 15 characters and contain only letters, numbers, and underscores</div>");
+        console.log("hi")
+        return false;
+    }
+
+}
